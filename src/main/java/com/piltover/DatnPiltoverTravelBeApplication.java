05c15fa1c005650f0b7d08cd624beb3a30fb7c2f
HayDT03 <101821636+haydt21@users.noreply.github.com>
2023-09-04 15:02:15 +0700

    init

---
+++ src/main/java/com/piltover/DatnPiltoverTravelBeApplication.java
@@ -0,0 +1,13 @@
+package com.piltover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnPiltoverTravelBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnPiltoverTravelBeApplication.class, args);
+	}
+
+}
