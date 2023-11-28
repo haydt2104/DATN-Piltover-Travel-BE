903db393a3f4570ed2886739825da440a1465f82
datvt2003 <tdat30404@gmail.com>
2023-11-28 18:51:17 +0700

    History commit day 28/11

---
+++ src/main/java/com/piltover/model/History.java
@@ -0,0 +1,44 @@
+package com.piltover.model;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+@Entity
+public class History {
+	@Id
+	private Long userID;
+    private String fullname;
+    private Long bookingID;
+    private Integer adult;
+    private Integer children;
+    private LocalDateTime bookDate;
+    private Integer totalPrice;
+    private Integer bookingStatus;
+    private String discountName;
+    private Double discountPercentage;
+    private String discountCode;
+    private Long tourID;
+    private String tourName;
+    private String tourDescription;
+	private String transportName;
+	private LocalDateTime startDate;
+    private String startAddress;
+    private LocalTime startTime;
+    private LocalTime endTime;
+}
--- src/main/java/com/piltover/repository/BookingRepository.java
+++ src/main/java/com/piltover/repository/BookingRepository.java
@@ -3,29 +3,50 @@ package com.piltover.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.piltover.entity.Booking;
+import com.piltover.model.History;
 
-public interface BookingRepository extends JpaRepository<Booking, Long>{
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 	// Get All data of bookings => (lấy tất cả dữ liệu trong Bookings)
-    @Query("SELECT b FROM Booking b")
-    List<Booking> findAll();
-    
-    // Get All data of bookings with status = 1(confirmed) => (Lấy tất danh sách khách đã liên hệ)
-    @Query("SELECT b FROM Booking b WHERE b.status = 1")
-    List<Booking> findAllConfirmed();
+	@Query("SELECT b FROM Booking b")
+	List<Booking> findAll();
 
-    // Get All data of bookings with status = 0(unconfirm) => (Lấy tất danh sách khách chưa liên hệ)
-    @Query("SELECT b FROM Booking b WHERE b.status = 0")
-    List<Booking> findAllUnConfirm();
+	// Get All data of bookings with status = 1(confirmed) => (Lấy tất danh sách
+	// khách đã liên hệ)
+	@Query("SELECT b FROM Booking b WHERE b.status = 1")
+	List<Booking> findAllConfirmed();
 
-    // Get All data of bookings with status = 1(confirmed) => (Lấy tất danh sách khách đã hủy)  
-    @Query("SELECT b FROM Booking b WHERE b.status = 2")
-    List<Booking> findAllCancel();
-    
-    @Query("SELECT bd FROM Booking bd WHERE bd.id=?1")
+	// Get All data of bookings with status = 0(unconfirm) => (Lấy tất danh sách
+	// khách chưa liên hệ)
+	@Query("SELECT b FROM Booking b WHERE b.status = 0")
+	List<Booking> findAllUnConfirm();
+
+	// Get All data of bookings with status = 1(confirmed) => (Lấy tất danh sách
+	// khách đã hủy)
+	@Query("SELECT b FROM Booking b WHERE b.status = 2")
+	List<Booking> findAllCancel();
+
+	@Query("SELECT bd FROM Booking bd WHERE bd.id=?1")
 	Booking findByBookingID(Long id);
+	
+	@Transactional
+	@Query(value = "CALL FindAllHistory()", nativeQuery = true)
+//    @Procedure(name="FindAllHistory")
+	List<Booking> History_ReadAll();
 
-    
+	@Modifying
+	@Transactional
+	@Query(value = "CALL ReadAllHistoryByAcc(:id)", nativeQuery = true)
+	List<Booking> ReadAllHistoryByAcc(Long id);
+
+//    @Transactional
+//    @Query(value = "CALL ReadOneHistoryByAcc`(:uid,:bid);",nativeQuery = true)
+//    Booking ReadoneHistoryByAcc(@Param("uid") Long uid,@Param("bid") Long bid);
+
 }
