c102c5929b6784cacd3d9d7f73ade19e96349d59
datvt2003 <tdat30404@gmail.com>
2023-12-14 03:04:26 +0700

    Update History(Cancel)

--- src/main/java/com/piltover/controller/user/HistoryBookingController.java
+++ src/main/java/com/piltover/controller/user/HistoryBookingController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.piltover.service.AccountService;
 import com.piltover.service.BookingService;
 import com.piltover.service.HistoryService;
+import com.piltover.util.ResponeUtil;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/user/history")
 public class HistoryBookingController {
+	
 	@Autowired
 	AccountService accService;
 	@Autowired
 	HistoryService historyService;
 	@Autowired
 	BookingService bookingService;
+	
+	@Autowired
+	ResponeUtil respUtill;
 
 	@GetMapping("/all")
 	private ResponseEntity<?> ReadAll() {
@@ -56,6 +62,21 @@ public class HistoryBookingController {
 		Long uid=accService.getId(mail);
 		
 		return ResponseEntity.ok(historyService.ReadOne(uid, p_bookingid));
+	}
+	
+	@DeleteMapping("/user/booking/cancel/{bid}")
+	public ResponseEntity<?> editBookingdetail(@PathVariable Long bid) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+
+		int newStatus = 2;
+		Long upUser = accService.getId(username);
+
+		bookingService.cancelBooking(bid, upUser, newStatus);
+
+		respUtill.putRespone("message", "Cancel booking susscess");
+
+		return ResponseEntity.ok(respUtill.getRespone());
 	}
 
 }
