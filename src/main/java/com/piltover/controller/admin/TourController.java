c280a682afb656a3087a5324ab92d40c260db09f
WeirdoJackDX <lhtho1803@gmail.com>
2023-12-17 00:56:11 +0700

    Update TourController.java

--- src/main/java/com/piltover/controller/admin/TourController.java
+++ src/main/java/com/piltover/controller/admin/TourController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.piltover.entity.Account;
 import com.piltover.entity.Tour;
+import com.piltover.service.AccountService;
 import com.piltover.service.TourService;
 
 @CrossOrigin("*")
@@ -23,6 +27,9 @@ import com.piltover.service.TourService;
 public class TourController {
     @Autowired
     TourService tourService;
+
+    @Autowired
+    AccountService accountService;
 
     @GetMapping("/tour/all")
     public ResponseEntity<List<Tour>> getAllTour() {
@@ -36,6 +43,7 @@ public class TourController {
 
     @PostMapping("/admin/tour")
     public void postTour(@RequestBody Tour tour) {
+        tour.setCreator(getUser());
         tourService.postTour(tour);
     }
 
@@ -47,5 +55,12 @@ public class TourController {
     @DeleteMapping("/admin/tour/{tourId}")
     public void deleteTour(@PathVariable Long tourId) {
         tourService.deleteTour(tourId);
+    }
+
+    public Account getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Long upUser = accountService.getId(username);
+        return accountService.findUserByID(upUser);
     }
 }
